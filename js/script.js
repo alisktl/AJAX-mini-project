c3f46030fe4a9704f672376548f137cdcb0b560c
Alisher <alisktl@gmail.com>
2015-11-02 17:52:02 +0600

    NY Times API is turned on

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    // Google StreetView
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
@@ -19,6 +20,21 @@ function loadData() {
 
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=3000x2500&location=' + address;
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // NY Times API
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=c556f8344cca5e46d14ef1ab008d032b:2:73358821';
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for(var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' +
+            '</li>');
+        }
+    });
 
     return false;
 };
